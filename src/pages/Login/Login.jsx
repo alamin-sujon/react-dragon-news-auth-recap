dd20be18df29c9c4ba0e74ee0e1363e711ddabe1
Al-amin07 <alsujon2001@gmail.com>
2024-04-05 09:39:47 +0600

    Login  Added

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div className="space-y-24">
+            <Navbar/>
+            This is Login
+        </div>
+    );
+};
+
+export default Login;
